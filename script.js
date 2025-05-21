c81b773c7e495ed96ad2e6eb8fa9177a3e65ad55
unknown <dinnifer.silva@aluno.senai.br>
2025-05-21 09:40:27 -0500

    namoro

---
+++ script.js
@@ -0,0 +1,9 @@
+function sim() {
+    alert ("Você aceitou namorar comigo! :)");
+
+    location.href = "https:youtu.be/fxtkVjBoDGg?si=JI9NjHCoD9z66lpd";
+}
+ function desvia(btn){
+    alert("Você nao me quis! :(")
+    btn.style.position = 'absolute';
+ }
